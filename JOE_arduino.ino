dc13f93f9f2351953db59c7d8f71012266ff7331
fatpandaclub <nhoejholdt@gmail.com>
2017-02-25 19:21:05 +0100

    Servos now work + milk handling

--- JOE_arduino.ino
+++ JOE_arduino.ino
@@ -20,9 +20,10 @@ void setup() {
   // initialize the pushbutton pin as an input:
   pinMode(leftButtonPin, INPUT_PULLUP);
   pinMode(rightButtonPin, INPUT_PULLUP);
-
+  
   // Attach servos to pin 2-11
   for(int i = 0; i < noOfServos; i++) {
+    servos[i].write(0);
     servos[i].attach(i+2);
   }
   
@@ -31,13 +32,36 @@ void setup() {
 void loop() {
 
   // Get info from Joe
-  if (Serial.available() > 0) { 
-    char input = Serial.parseInt();
+  if (Serial.available() > 0) {
+    String readInput = Serial.readStringUntil('\n');
+    int input = readInput.toInt();
+    
     // Test that input is within servo range
-    if(input < 0 || input > noOfServos) return;
+    if(input < 0 || input > 5) return;
+    
     servos[input].write(180);
     delay(1000);
     servos[input].write(0);
+    delay(1000);
+    
+    // Drink 2, 4, 5 need milk
+    switch (input) {
+      case 2:
+        servos[6].write(180);
+        delay(1000);
+        servos[6].write(0);
+        break;
+      case 4:
+        servos[7].write(180);
+        delay(1000);
+        servos[7].write(0);
+        break;
+      case 5:
+        servos[8].write(180);
+        delay(1000);
+        servos[8].write(0);
+        break;
+    }    
   }
 
   // SEND INFO
